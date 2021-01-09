3c695c9831e2809e56be89813ab74acdbaf9214b
enock24 <enookimuu@gmail.com>
2021-01-09 23:25:58 +0300

    add class location to models.py

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+import datetime as dt
+
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+    
+    def save_location(self):
+        self.save()
+        
+    def delete_location(self):
+        self.delete()
+        
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(name = value)
+        
+    def __str__(self):
+        return self.name 
+    
