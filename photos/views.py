03dd60c0a6c7617713f6556d37225ec30b2994ee
enock24 <enookimuu@gmail.com>
2021-01-09 13:43:30 +0300

    add 404 erro to views.py

--- photos/views.py
+++ photos/views.py
@@ -32,7 +32,15 @@ def convert_dates(dates):
 
 def past_days_photos(request,past_date):
         # Converts data from the string Url
+       def past_days_photos(request,past_date):
+
+    try:
+        # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
 
     day = convert_dates(date)
     html = f'''
