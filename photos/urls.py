f8beb33834583d77fe67d9a57d8a8d3a7c0bb237
enock24 <enookimuu@gmail.com>
2021-01-09 16:29:30 +0300

    add photos.app to settings.py

--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns =[
     url(r'^$', views.welcome, name = 'welcome'),
-    url(r'^today/$', views.news_of_day, name = 'newsToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews') 
+    url(r'^today/$', views.photos_of_day, name = 'photosToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastphotos') 
 ]
