f4ea784484e44118c332f543579d90a7b52a55fc
enock24 <enookimuu@gmail.com>
2021-01-09 13:11:50 +0300

    add news of the day views

--- photos/views.py
+++ photos/views.py
@@ -7,11 +7,25 @@ def welcome(request):
 
 def photos_of_day(request):
     date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
+                <h1>photos for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
-    return HttpResponse(html)   
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day    
