a9c3ac9806bd67e17d7376565a4298917cf58b4e
enock24 <enookimuu@gmail.com>
2021-01-09 16:14:34 +0300

    add import to views.py

--- photos/views.py
+++ photos/views.py
@@ -1,58 +1,28 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
 import datetime as dt
-from django.http  import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to the Gallery Page')
+    return render(request, 'welcome.html')
 
 def photos_of_day(request):
     date = dt.date.today()
+    return render(request, 'all-photos/today-photos.html',{'date': date})
 
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>photos for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
 
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day  
 
 def past_days_photos(request,past_date):
-        # Converts data from the string Url
-       def past_days_photos(request,past_date):
-
+    
     try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>photos for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(photos_of_day)
 
-# Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')    
+    return render(request, 'all-photos/past-photos.html', {"date":date})   
