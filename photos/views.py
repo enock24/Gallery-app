b36609183a2bd969c53a63f808170a2ec7cc7e52
enock24 <enookimuu@gmail.com>
2021-01-09 13:02:00 +0300

    add urls to photos app

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,17 @@
+import datetime as dt
 from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Gallery Page')
+
+def photos_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)   
