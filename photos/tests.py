c8eeab7f6a823a53057cd0bbf46828e8221847cd
enock24 <enookimuu@gmail.com>
2021-01-10 09:51:28 +0300

    add test class for location

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Location,Image,Category
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.location = Location(id=1,name = 'Nairobi')
+        
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+        
