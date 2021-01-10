ce7bc7e5cc5b495cd67cb1fdb94214e99da4a37c
enock24 <enookimuu@gmail.com>
2021-01-10 09:58:01 +0300

    add urls for gall views

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.urls import path,re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns =[
-    url(r'^$', views.welcome, name = 'welcome'),
-    url(r'^today/$', views.photos_of_day, name = 'photosToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastphotos') 
+    path('', views.my_gallery, name = 'myGallery'),
+    path('search/', views.search_results, name='search_results'),
+    re_path('search/location', views.search_location, name='search_location'),
 ]
