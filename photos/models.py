2b2a61bbc0de88e423df381bccbbc22f04375606
enock24 <enookimuu@gmail.com>
2021-01-10 09:47:04 +0300

    add class category to models.py

--- photos/models.py
+++ photos/models.py
@@ -20,4 +20,21 @@ class Location(models.Model):
         
     def __str__(self):
         return self.name 
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    
+    def save_category(self):
+        self.save()
+        
+    def delete_category(self):
+        self.delete()
+        
+    @classmethod
+    def update_category(cls, id, value):
+        cls.objects.filter(id=id).update(name = value)
+        
+    def __str__(self):
+        return self.name 
+                
     
