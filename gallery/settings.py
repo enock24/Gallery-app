ce0ac3cdf49412670cfa80de88bb79168242ff35
enock24 <enookimuu@gmail.com>
2021-01-10 10:09:07 +0300

    add all imports to settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -10,8 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
